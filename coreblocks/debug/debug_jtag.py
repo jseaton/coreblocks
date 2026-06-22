9ad85d5619037790019ffc2849e30b2cca59a07c
Jo Seaton <coreblocks@syscall.moe>
2026-06-22 12:05:58 +0100

    JTAG WIP

--- coreblocks/debug/debug_jtag.py
+++ coreblocks/debug/debug_jtag.py
@@ -15,9 +15,9 @@ from transactron import *
 class JTAGDebugInterface(ComponentInterface):
     def __init__(self):
         self.tck = CIn() # TODO magic clock?
-        self.tms = CIn(32)
-        self.tdi = CIn(32)
-        self.tdo = COut(32)
+        self.tms = CIn()
+        self.tdi = CIn()
+        self.tdo = COut()
 
 class DebugJTAGTAP(Component):
     jtag: JTAGDebugInterface
@@ -28,12 +28,173 @@ class DebugJTAGTAP(Component):
     def elaborate(self, platform):
         m = TModule()
 
-        self.jtag.tck = Signal()
-        self.jtag.tms = Signal(32)
-        self.jtag.tdi = Signal(32)
-        self.jtag.tdo = Signal(32)
+        dtmcs_layout = StructLayout({
+            "version": 4,
+            "abits": 6,
+            "dmistat":  2,
+            "idle": 3,
+            "0" : 1,
+            "dmireset" : 1,
+            "dtmhardreset" : 1,
+            "errinfo" : 3
+        })
 
-        m.d.sync += self.jtag.tdo.eq(self.jtag.tdo + 1)
+        m.domains.jtag_pos = cd_jtag_pos = ClockDomain()
+        m.domains.jtag_neg = cd_jtag_neg = ClockDomain()
+        m.d.comb += cd_jtag_pos.clk.eq(self.jtag.tck)
+        m.d.comb += cd_jtag_neg.clk.eq(~self.jtag.tck)
+
+        ir = Signal(5)
+        dr = Signal(33+32)
+
+        read = Signal()
+        write = Signal()
+
+        dtmcs = Signal(32)
+        dmi = Signal(32)
+
+        dmi_op = Signal(2)
+
+        wtf = Signal(3)
+
+        width = {
+                0x0 : 1, 0x12 : 1, 0x13 : 1, 0x14 : 1, 0x15 : 1, 0x15 : 1, 0x17 : 1, 0x1f : 1,
+                0x1 : 32,
+                0x10 : 32,
+                0x11 : 33+32
+                }
+
+        with m.FSM(domain="jtag_pos"):
+            with m.State("Test-Logic-Reset"): # 0
+                m.d.comb += wtf.eq(0)
+                with m.If(~self.jtag.tms):
+                    m.next = "Idle"
+                m.d.jtag_pos += ir.eq(1)
+            with m.State("Idle"): # 1
+                m.d.comb += wtf.eq(1)
+                with m.If(self.jtag.tms):
+                    m.next = "Select-DR-Scan"
+
+            with m.State("Select-DR-Scan"): # 2
+                m.d.comb += wtf.eq(2)
+                with m.If(self.jtag.tms):
+                    m.next = "Select-IR-Scan"
+                with m.Else():
+                    m.next = "Capture-DR"
+            with m.State("Capture-DR"): # 3
+                m.d.comb += wtf.eq(3)
+                with m.If(self.jtag.tms):
+                    m.next = "Exit1-DR"
+                with m.Else():
+                    m.next = "Shift-DR"
+                m.d.comb += read.eq(1)
+            with m.State("Shift-DR"): # 4
+                m.d.comb += wtf.eq(4)
+                m.d.comb += read.eq(0)
+                with m.If(self.jtag.tms):
+                    m.next = "Exit1-DR"
+                with m.Else():
+                    m.d.jtag_neg += self.jtag.tdo.eq(0)
+                    for k,v in width.items(): # TODO is there a nicer way to do this?
+                        with m.If(ir == k):
+                            m.d.jtag_neg += self.jtag.tdo.eq(dr[0])
+                            m.d.jtag_pos += dr.eq(Cat(dr[1:v], self.jtag.tdi))
+            with m.State("Exit1-DR"): # 5
+                m.d.comb += wtf.eq(5)
+                with m.If(self.jtag.tms):
+                    m.next = "Update-DR"
+                with m.Else():
+                    m.next = "Pause-DR"
+            with m.State("Pause-DR"):
+                m.d.comb += wtf.eq(6)
+                with m.If(self.jtag.tms): # 6
+                    m.next = "Exit2-DR"
+            with m.State("Exit2-DR"): # 7
+                m.d.comb += wtf.eq(7)
+                with m.If(self.jtag.tms):
+                    m.next = "Update-DR"
+                with m.Else():
+                    m.next = "Shift-DR"
+            with m.State("Update-DR"): # 8
+                m.d.comb += wtf.eq(8)
+                # TODO write
+                with m.If(self.jtag.tms):
+                    m.next = "Select-DR-Scan"
+                with m.Else():
+                    m.next = "Idle"
+
+            with m.State("Select-IR-Scan"): # 9
+                m.d.comb += wtf.eq(9)
+                with m.If(self.jtag.tms):
+                    m.next = "Test-Logic-Reset"
+                with m.Else():
+                    m.next = "Capture-IR"
+            with m.State("Capture-IR"): # a
+                m.d.comb += wtf.eq(0xa)
+                m.d.jtag_pos += ir.eq(1)
+                with m.If(self.jtag.tms):
+                    m.next = "Exit1-IR"
+                with m.Else():
+                    m.next = "Shift-IR"
+            with m.State("Shift-IR"): # b
+                m.d.comb += wtf.eq(0xb)
+                with m.If(self.jtag.tms):
+                    m.next = "Exit1-IR"
+                with m.Else():
+                    m.d.jtag_neg += self.jtag.tdo.eq(ir[0])
+                    m.d.jtag_pos += ir.eq(Cat(ir[1:], self.jtag.tdi))
+            with m.State("Exit1-IR"): # c
+                m.d.comb += wtf.eq(0xc)
+                with m.If(self.jtag.tms):
+                    m.next = "Update-IR"
+                with m.Else():
+                    m.next = "Pause-IR"
+            with m.State("Pause-IR"): # d
+                m.d.comb += wtf.eq(0xd)
+                with m.If(self.jtag.tms):
+                    m.next = "Exit2-IR"
+            with m.State("Exit2-IR"): # e
+                m.d.comb += wtf.eq(0xe)
+                with m.If(self.jtag.tms):
+                    m.next = "Update-IR"
+                with m.Else():
+                    m.next = "Shift-IR"
+            with m.State("Update-IR"): # f
+                m.d.comb += wtf.eq(0xf)
+                with m.If(self.jtag.tms):
+                    m.next = "Select-DR-Scan"
+                with m.Else():
+                    m.next = "Idle"
+
+        with m.If(read), m.Switch(ir):
+            with m.Case(0x0, 0x12, 0x13, 0x14, 0x15, 0x15, 0x17, 0x1f): # bypass
+                m.d.jtag_pos += dr.eq(0)
+            with m.Case(0x1): # idcode
+                m.d.jtag_pos += dr.eq(0x10003FFF)
+            with m.Case(0x10): # dtmcs
+                resp = Signal(dtmcs_layout) # TODO use View
+                m.d.comb += [
+                        resp.version.eq(1),
+                        resp.abits.eq(32),
+                        resp.dmistat.eq(dmi_op),
+                        resp.idle.eq(10)
+                        ]
+                m.d.jtag_pos += dr.eq(resp)
+            with m.Case(0x11): # dmi
+                m.d.jtag_pos += dr.eq(dmi)
+
+        with m.If(write), m.Switch(ir):
+            with m.Case(0x0, 0x1, 0x12, 0x13, 0x14, 0x15, 0x15, 0x17, 0x1f): # bypass, idcode
+                pass # m.d.jtag_neg += rsp_op.eq(2) TODO error?
+            with m.Case(0x10): # dtmcs
+                req = Signal(dtmcs_layout) # TODO use View
+                m.d.jtag_neg += req.eq(dr)
+                with m.If(req.dmireset): # TODO
+                    pass
+                with m.If(req.dtmhardreset): # TODO
+                    pass
+            with m.Case(0x11): # dtmcs
+                m.d.jtag_neg += dmi.eq(dr)
 
 #        address = Signal(32) # TODO size
 #        data = Signal(32)
